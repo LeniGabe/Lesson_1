873ed7807c8429101c849e0b210c179ceac3dafc
«Elena» <lenigabe8@gmail.com>
2022-11-17 23:11:45 +0700

    add Написана программа по первой задаче

---
+++ Задача_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if(a > b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine("Наибольшее число равно " + a);
+}
+else
+{
+    max = b;
+    min = a;
+    Console.WriteLine("Наибольшее число равно " + b);
+}
