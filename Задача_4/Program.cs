aaa908ac9bd52e213be602d56e0aa39a585672ba
«Elena» <lenigabe8@gmail.com>
2022-11-20 11:50:08 +0700

    add Добавлено 2 задачи и прописаны условия

--- Задача_4/Program.cs
+++ Задача_4/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число a");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число a");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число b");
